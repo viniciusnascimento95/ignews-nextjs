7e3ecf2b3171ad1577790a3e66c2653efa4617db
vinicius.souza <vinicius.souza@smartsupport.com.br>
2022-01-04 23:32:33 -0300

    create component header

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h2>teste</h2>
+    </>
+  )
+}
