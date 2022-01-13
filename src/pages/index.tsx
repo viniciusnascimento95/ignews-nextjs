963dda06671d5bbf3872382af2d73a5ada711d00
Vinicius <vinicius.souza@smartsupport.com.br>
2022-01-12 23:18:40 -0300

    update front

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../../styles/Home.module.scss'
+import Head from 'next/head';
+
 
 export default function Home() {
   return (
@@ -8,7 +7,7 @@ export default function Home() {
       <Head>
         <title>ig.news</title>
       </Head>
-      <h2>teste</h2>
+      <h2>Olá mundo</h2>
     </>
   )
 }
