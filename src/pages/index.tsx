a3da2f8bb6e8f6745837b5b057e12db9a45e01d9
vinicius <nascimento.vinicius32@gmail.com>
2022-01-16 23:07:44 -0300

    update frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
 
 
 export default function Home() {
@@ -7,7 +10,20 @@ export default function Home() {
       <Head>
         <title>ig.news</title>
       </Head>
-      <h2>Olá mundo</h2>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>News about the <span>React</span> world</h1>
+
+          <p>Get access to all the publications <br />
+          <span>for $9.99 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
